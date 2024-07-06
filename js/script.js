444165a4c1f1fa0c2d6ffa79de5008e038c1b9a8
iamoliverden <90800882+iamoliverden@users.noreply.github.com>
2024-07-06 17:06:52 -0400

    Project Launch

--- js/script.js
+++ js/script.js
@@ -1,3 +1,69 @@
-$(window).on('scroll', function() {
-  $('.upper-quarter').css('background-position', '0 -' + $(this).scrollTop() / 2 + 'px');
+
+// Links
+document.getElementById('batmanology').addEventListener('click', function(event) {
+    window.location.href = 'https://fansofbatman.com/';
+});
+
+
+document.getElementById('clinics').addEventListener('click', function(event) {
+    window.location.href = 'https://www.verywellmind.com/delusional-disorder-symptoms-causes-and-treatment-6826265';
+});
+
+document.getElementById('kitchen').addEventListener('click', function(event) {
+    window.location.href = 'https://www.ubereats.com/ca/store/moms-kitchen-korean-cuisine-markham/OrH5toqhQRCFVHOga3P_gw';
+});
+
+document.getElementById('jre').addEventListener('click', function(event) {
+    window.location.href = 'https://www.youtube.com/user/PowerfulJRE';
+});
+
+document.getElementById('jre').addEventListener('click', function(event) {
+    window.location.href = 'https://www.youtube.com/user/PowerfulJRE';
+});
+
+document.getElementById('kickboxer').addEventListener('click', function(event) {
+    window.location.href = 'https://www.wkfworld.com/ranking/world-ranking-men';
+});
+
+
+document.getElementById('gymshark').addEventListener('click', function(event) {
+    window.location.href = 'https://ca.gymshark.com';
+});
+
+
+document.getElementById('gymshark').addEventListener('click', function(event) {
+    window.location.href = 'https://ca.gymshark.com';
+});
+
+
+document.getElementById('israel').addEventListener('click', function(event) {
+    window.location.href = 'https://www.haaretz.com/';
+});
+
+
+document.getElementById('soccer').addEventListener('click', function(event) {
+    window.location.href = 'https://www.cnn.com/2022/11/26/football/football-soccer-history-world-cup-spt-intl/index.html';
+});
+
+
+// confirmation
+
+// Get the modal dialog and the buttons
+var modal = document.getElementById('myModal');
+var yesBtn = document.getElementById('yesBtn');
+var noBtn = document.getElementById('noBtn');
+
+// When the user clicks on the antisocial div, open the modal
+document.getElementById('antisocial').addEventListener('click', function(event) {
+    modal.style.display = 'flex';
+});
+
+// When the user clicks on "Yes", redirect to the contact form
+yesBtn.addEventListener('click', function(event) {
+    window.location.href = 'your-contact-form-url-here';
+});
+
+// When the user clicks on "Cancel", close the modal
+noBtn.addEventListener('click', function(event) {
+    modal.style.display = 'none';
 });
